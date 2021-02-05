50a00fd8edbeedf3a0969d7b179509272211e223
Roseanne Nguyen <rtnguye1@gmail.com>
2021-02-04 21:00:03 -0700

    install uuid npm

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const uuid = require('uuid');
 
 
 var tableData = require("./db/db.json");
 
-// app.get("/api/notes", function (req, res) {
-//     res.json(tableData);
-// });
+app.get("/api/notes", function (req, res) {
+    res.json(tableData);
+});
 
-// app.post("/api/notes", function (req, res) {
+app.post("/api/notes", function (req, res) {
 
-//         tableData.push(req.body);
-//         res.json(true);
+        tableData.push(req.body);
+        res.json(true);
 
-// });
+});
+
+
+app.delete("/api/notes/:id", function (req, res) {
+
+    tableData.push(req.body);
+    res.json(true);
+
+});
